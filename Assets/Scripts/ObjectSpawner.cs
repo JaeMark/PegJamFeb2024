35088110da6f8878e94735572b7f55abf975d35a
Jetro De Leon <j08mark@hotmail.com>
2024-02-24 02:12:12 -0600

    Created zombee variants with a weighted probability to spawn

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [System.Serializable]
@@ -17,8 +18,15 @@ public struct RandomForceAndAngle
 
 public class ObjectSpawner : MonoBehaviour
 {
+    [System.Serializable]
+    public struct ObjectPrefabWeight
+    {
+        public GameObject prefab;
+        public float weight;
+    }
+
     [SerializeField]
-    private GameObject[] objectPrefabs;
+    private ObjectPrefabWeight[] objectPrefabs;
 
     [SerializeField]
     private RandomForceAndAngle forceAndAngle;
@@ -29,7 +37,15 @@ public class ObjectSpawner : MonoBehaviour
     [SerializeField]
     private float xOffset = 5.0f;
 
+    private float totalWeight; // Total weight of all object prefabs
+
     private float timer = 0f;
+
+    private void Start()
+    {
+        // Calculate the total weight of all object prefabs
+        totalWeight = objectPrefabs.Sum(obj => obj.weight);
+    }
 
     private void Update()
     {
@@ -47,8 +63,11 @@ public class ObjectSpawner : MonoBehaviour
         // Apply offset to the random x position
         float randomX = Random.Range(-xOffset, xOffset);
 
-        // Choose a random object prefab from the array
-        GameObject objectPrefab = objectPrefabs[Random.Range(0, objectPrefabs.Length)];
+        // Generate a random value within the total weight range
+        float randomWeight = Random.Range(0f, totalWeight);
+
+        // Choose the object prefab based on the weighted probability
+        GameObject objectPrefab = ChooseObjectPrefab(randomWeight);
 
         // Instantiate a new object at the random x position
         GameObject newObject = Instantiate(objectPrefab, new Vector3(randomX, transform.position.y, transform.position.z), Quaternion.identity);
@@ -59,5 +78,27 @@ public class ObjectSpawner : MonoBehaviour
         float angle = Random.Range(forceAndAngle.minAngle, forceAndAngle.maxAngle);
         Vector2 forceVector = Quaternion.Euler(0, 0, angle) * Vector2.right * force;
         rb.AddForce(forceVector, ForceMode2D.Impulse);
+    }
+
+    private GameObject ChooseObjectPrefab(float randomWeight)
+    {
+        float cumulativeWeight = 0f;
+
+        // Iterate over each object prefab and choose based on weighted probability
+        foreach (var obj in objectPrefabs)
+        {
+            cumulativeWeight += obj.weight;
+
+            // If the random weight falls within the current cumulative weight,
+            // choose this object prefab
+            if (randomWeight <= cumulativeWeight)
+            {
+                return obj.prefab;
+            }
+        }
+
+        // This should not happen under normal circumstances, but just in case,
+        // return the last object prefab in the array
+        return objectPrefabs[objectPrefabs.Length - 1].prefab;
     }
 }
